4d27fd67d148d860724172127392c1f5713d74f5
Bunit73 <ford2bm@gmail.com>
2017-05-26 15:07:18 -0400

    basic node chart

--- static/js/nodeChart.js
+++ static/js/nodeChart.js
@@ -1,3 +1,69 @@
+var treeData =
+    	{
+		"name" : "Top Level",
+		"children" : [
+		{
+			"name" : "Level 2 A",
+			"children" : [
+				{"name" : "A1"},
+				{"name" : "A2"}
+			]
+		},
+		{	"name" : "Level 2 B",
+			"children" : [
+				{"name" : "B1"},
+				{"name" : "B2"},
+				{"name" : "B3"}
+			]
+		},
+		{
+			"name" : "Level 2 C",
+			"children" : [{"name" : "C1"}]
+		}
+		]
+    	};
+
+//Set canvas specs
+//Set canvas size
+var margin = {top: 20, right: 100, bottom: 20, left: 80};
+var height = 620 + margin.top + margin.bottom;
+var width = 780 + margin.left + margin.right;
+var duration = 750;
+
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .style("background-color","#f7f7f9");
+
+// append the svg to the html chrt
+var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//make the tree map object
+var treemap = d3.tree().size([width,height]);
+
+//links the data
+var nodes = d3.hierarchy(treeData, function(d){
+    return d.children;
+});
+
+//maps the nodes the the lay out
+nodes = treemap(nodes);
+
+//add the nodes
+// adds each node as a group
+var node = g.selectAll(".node")
+    .data(nodes.descendants())
+    .enter().append("g")
+    .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+    .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+// adds the circle to the node
+node.append("circle").attr("r", 10);
+
+//Tab functions
 function showChartArea() {
 
     $("#chart-area").show();

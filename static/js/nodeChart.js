81af438585d821388b8e5565178697c690ff4a97
Bunit73 <ford2bm@gmail.com>
2017-06-01 13:30:41 -0400

    testb

--- static/js/nodeChart.js
+++ static/js/nodeChart.js
@@ -1,57 +1,8 @@
-var treeData = {};
-// var treeData =
-//     	{
-// 		"name" : "Top Level",
-// 		"children" : [
-// 		{
-// 			"name" : "Level 2 A",
-// 			"children" : [
-// 				{"name" : "A1"},
-// 				{"name" : "A2"}
-// 			]
-// 		},
-// 		{	"name" : "Level 2 B",
-// 			"children" : [
-// 				{"name" : "B1"},
-// 				{"name" : "B2"},
-// 				{"name" : "B3"}
-// 			]
-// 		},
-// 		{
-// 			"name" : "Level 2 C",
-// 			"children" : [{"name" : "C1"}]
-// 		}
-// 		]
-//     	};
-
-// var treeData2 =
-//     	{
-// 		"name" : "Top Level",
-// 		"children" : [
-// 		{
-// 			"name" : "Level 2 A",
-// 			"children" : [
-// 				{"name" : "A1"},
-// 				{"name" : "A2"}
-// 			]
-// 		},
-// 		{	"name" : "Level 2 B",
-// 			"children" : [
-// 				{"name" : "B1"},
-// 				{"name" : "B2"},
-// 				{"name" : "B3"}
-// 			]
-// 		},
-// 		{
-// 			"name" : "Level 2 C",
-// 			"children" : [{"name" : "C1"}]
-// 		},
-//             {
-// 			"name" : "Level 2 D",
-// 			"children" : []
-// 		}
-// 		]
-//     	};
+var treeData = {'root':"http://web.engr.oregonstate.edu/~mjb/cs575e/",
+                'name':"http://web.engr.oregonstate.edu/~mjb/cs575e/",
+                'children': [],
+                'status': "Keyword Found"
+            };
 
 //Set canvas specs
 //Set canvas size
@@ -59,6 +10,9 @@ var margin = {top: 20, right: 100, bottom: 20, left: 80};
 var height = 620 + margin.top + margin.bottom;
 var width = 780 + margin.left + margin.right;
 var duration = 750;
+var i = 0;
+
+var treemap = d3.tree().size([height, width]);
 
 // append the svg obgect to the body of the page
 // appends a 'group' element to 'svg'
@@ -66,102 +20,163 @@ var duration = 750;
 var svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-                .call(d3.zoom()
-                .scaleExtent([1 / 2, 4])
-                .on("zoom", zoomed))
     .style("background-color","#f7f7f9");
 
-// https://bl.ocks.org/mbostock/2a39a768b1d4bc00a09650edef75ad39
-function zoomed() {
-  var transform = d3.event.transform;
-  //transform the nodes
-    g.selectAll(".node").attr("transform", function(d) {
-        return "translate(" + transform.applyX(d.x) + "," + transform.applyY(d.y) + ")";
-  });
-}
+// Assigns parent, children, height, depth
+root = d3.hierarchy(treeData, function(d) { return d.children; });
+root.x0 = 100;
+root.y0 = 100;
 
-// append the svg to the html chart
-var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+update(root);
 
-
-//make the node map object
-var nodeMap = d3.tree().size([width,height]);
-
-//links the data
-var nodes = d3.hierarchy(treeData, function(d){
-    return d.children;
-});
-
-//maps the nodes the the lay out
-nodes = nodeMap(nodes);
-
-//add the nodes
-// adds each node as a group
-var node = g.selectAll(".node")
-    .data(nodes.descendants())
-    .enter().append("g")
-    .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-    .on("mouseover",function (d) {
-            console.log(d);
-    });
-
-// adds the circle to the node
-node.append("circle").attr("r", 10);
-
-
-function update(newNode) {
-    if(Object.keys(treeData).length === 0){
-        treeData = newNode;
-        nodes.data = newNode;
-        nodes.children = [];
-        nodes.id = newNode.url;
-        //console.log(nodes);
-    }
-    else{
-        //console.log(newNode);
-        //console.log(search(nodes,newNode.root));
-        var p = search(nodes,newNode.root);
-        if(p){
-            temp_node = d3.hierarchy(newNode, function(d){
-                    return d.children;
-            });
-
-            temp_node.children = [];
-            temp_node.id = newNode.url;
-            //TODO: find level for spacing
-            temp_node.x = 100;
-            temp_node.y = 200;
-            temp_node.parent = p;
-            p.children.push(temp_node);
-
-            node = g.selectAll(".node")
-                .data(p.descendants())
-                .enter().append("g")
-                .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-                .attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")"; })
-                .on("mouseover",function (d) {
-                        console.log(d);
-                });
-
-            node.append("circle").attr("r", 10);
-
-        }
-    }
-
-}
-//https://stackoverflow.com/questions/26317004/how-to-get-web-element-id-from-the-element-position-in-d3-js-force-graph
-function search(root, id) {
-  var found;
-  function recurse(node) {
-    if (node.id === id)
-      found = node;
-    !found && node.children && node.children.forEach(function(child) {
-      recurse(child);
-    });
+// Collapse the node and all it's children
+function collapse(d) {
+  if(d.children) {
+    d._children = d.children
+    d._children.forEach(collapse)
+    d.children = null
   }
-  recurse(root);
-  return found;
+}
+
+function update(source) {
+
+  // Assigns the x and y position for the nodes
+  var treeData = treemap(root);
+
+  // Compute the new tree layout.
+  var nodes = treeData.descendants(),
+      links = treeData.descendants().slice(1);
+
+  // Normalize for fixed-depth.
+  nodes.forEach(function(d){ d.y = d.depth * 180});
+
+  // ****************** Nodes section ***************************
+
+  // Update the nodes...
+  var node = svg.selectAll('g.node')
+      .data(nodes, function(d) {return d.id || (d.id = ++i); });
+
+  // Enter any new modes at the parent's previous position.
+  var nodeEnter = node.enter().append('g')
+      .attr('class', 'node')
+      .attr("transform", function(d) {
+        return "translate(" + source.y0 + "," + source.x0 + ")";
+    })
+    .on('click', click);
+
+  // Add Circle for the nodes
+  nodeEnter.append('circle')
+      .attr('class', 'node')
+      .attr('r', 1e-6)
+      .style("fill", function(d) {
+          return d._children ? "lightsteelblue" : "#fff";
+      });
+
+  // Add labels for the nodes
+  // nodeEnter.append('text')
+  //     .attr("dy", ".35em")
+  //     .attr("x", function(d) {
+  //         return d.children || d._children ? -13 : 13;
+  //     })
+  //     .attr("text-anchor", function(d) {
+  //         return d.children || d._children ? "end" : "start";
+  //     })
+  //     .text(function(d) { return d.data.name; });
+
+  // UPDATE
+  var nodeUpdate = nodeEnter.merge(node);
+
+  // Transition to the proper position for the node
+  nodeUpdate.transition()
+    .duration(duration)
+    .attr("transform", function(d) {
+        return "translate(" + d.y + "," + d.x + ")";
+     });
+
+  // Update the node attributes and style
+  nodeUpdate.select('circle.node')
+    .attr('r', 10)
+    .style("fill", function(d) {
+        return d._children ? "lightsteelblue" : "#fff";
+    })
+    .attr('cursor', 'pointer');
+
+
+  // Remove any exiting nodes
+  var nodeExit = node.exit().transition()
+      .duration(duration)
+      .attr("transform", function(d) {
+          return "translate(" + source.y + "," + source.x + ")";
+      })
+      .remove();
+
+  // On exit reduce the node circles size to 0
+  nodeExit.select('circle')
+    .attr('r', 1e-6);
+
+  // On exit reduce the opacity of text labels
+  nodeExit.select('text')
+    .style('fill-opacity', 1e-6);
+
+  // ****************** links section ***************************
+
+  // Update the links...
+  var link = svg.selectAll('path.link')
+      .data(links, function(d) { return d.id; });
+
+  // Enter any new links at the parent's previous position.
+  var linkEnter = link.enter().insert('path', "g")
+      .attr("class", "link")
+      .attr('d', function(d){
+        var o = {x: source.x0, y: source.y0}
+        return diagonal(o, o)
+      });
+
+  // UPDATE
+  var linkUpdate = linkEnter.merge(link);
+
+  // Transition back to the parent element position
+  linkUpdate.transition()
+      .duration(duration)
+      .attr('d', function(d){ return diagonal(d, d.parent) });
+
+  // Remove any exiting links
+  var linkExit = link.exit().transition()
+      .duration(duration)
+      .attr('d', function(d) {
+        var o = {x: source.x, y: source.y}
+        return diagonal(o, o)
+      })
+      .remove();
+
+  // Store the old positions for transition.
+  nodes.forEach(function(d){
+    d.x0 = d.x;
+    d.y0 = d.y;
+  });
+
+  // Creates a curved (diagonal) path from parent to the child nodes
+  function diagonal(s, d) {
+
+    path = `M ${s.y} ${s.x}
+            C ${(s.y + d.y) / 2} ${s.x},
+              ${(s.y + d.y) / 2} ${d.x},
+              ${d.y} ${d.x}`
+
+    return path
+  }
+
+  // Toggle children on click.
+  function click(d) {
+    if (d.children) {
+        d._children = d.children;
+        d.children = null;
+      } else {
+        d.children = d._children;
+        d._children = null;
+      }
+    update(d);
+  }
 }
 
 //Tab functions
@@ -192,19 +207,16 @@ $(document).ready(function () {
     });
     //console.log(nodes)
 
-    update({'root':"http://web.engr.oregonstate.edu/~mjb/cs575e/",
-            'url':"http://web.engr.oregonstate.edu/~mjb/cs575e/",
-            'status': "Keyword Found"
-    });
+    //update();
+    //
+    // function addNode() {
+    //     update({'root':"http://web.engr.oregonstate.edu/~mjb/cs575e/",
+    //         'url':"test",
+    //         'status': "OK"
+    //    });
+    // }
 
-    function addNode() {
-        update({'root':"http://web.engr.oregonstate.edu/~mjb/cs575e/",
-            'url':"test",
-            'status': "OK"
-       });
-    }
-
-        setTimeout(addNode,1000);
+        // setTimeout(addNode,1000);
 
 
 
